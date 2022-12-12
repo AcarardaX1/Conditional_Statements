111bb312baff39fe11428f7582852fd5c860e975
Arda Acar <108298837+AcarardaX1@users.noreply.github.com>
2022-12-12 14:05:02 +0100

    Add files via upload

---
+++ Logical-Conditional_Statements.py
@@ -0,0 +1,32 @@
+is_hot = False #Specify if it's a hot day or not
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif not is_hot:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("Do whatever you want")
+    
+print("Enjoy your day")
+
+
+print("----------------------------------------------------------------")
+ 
+price = 1000000
+good_score4credit = True #Specify if the credit score is good or not
+
+if good_score4credit:
+    down_payment = 0.1 * price
+else:
+    down_payment= 0.2 * price
+print(f"Down payment: ${down_payment}")
+
+
+print("----------------------------------------------------------------")
+
+good_credit=True #Specify if the credit score is good or not
+criminal_record=True #Specify if the person has a criminal record or not
+if good_credit and not criminal_record:
+    print("Eligible for loan")
